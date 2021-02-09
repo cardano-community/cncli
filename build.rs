d9d6068503a6f1a423c77acfc1c3c4a841f0c0f6
Michael Fazio <michaelfazio@me.com>
2021-02-09 14:39:13 +0800

    Ensure run with autoreconf -vfi flags

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use autotools;
 
 fn main() {
     // Build and link IOHK libsodium
-    let libsodium = autotools::Config::new("contrib/libsodium/").reconf("-v").build();
+    let libsodium = autotools::Config::new("contrib/libsodium/").reconf("-vfi").build();
     println!("cargo:rustc-link-search=native={}", libsodium.join("lib").display());
     println!("cargo:rustc-link-lib=static=sodium");
 
