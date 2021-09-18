ba5902ccb80b53a19330a6cbfe960183c13c91aa
Andrew Westberg <andrewwestberg@gmail.com>
2021-09-18 02:04:43 +0000

    Update Scripts

---
+++ scripts/cncli-leaderlog-prev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export CARDANO_NODE_SOCKET_PATH=/home/westbam/haskell/local/db/socket
+
+/home/westbam/.cargo/bin/cncli sync --host 127.0.0.1 --port 6000 --no-service
+echo "BCSH"
+SNAPSHOT=$(/home/westbam/.local/bin/cardano-cli query stake-snapshot --stake-pool-id 00beef0a9be2f6d897ed24a613cf547bb20cd282a04edfc53d477114 --mainnet)
+POOL_STAKE=$(echo "$SNAPSHOT" | grep -oP '(?<=    "poolStakeGo": )\d+(?=,?)')
+ACTIVE_STAKE=$(echo "$SNAPSHOT" | grep -oP '(?<=    "activeStakeGo": )\d+(?=,?)')
+BCSH=`/home/westbam/.cargo/bin/cncli leaderlog --pool-id 00beef0a9be2f6d897ed24a613cf547bb20cd282a04edfc53d477114 --pool-vrf-skey ./bcsh.vrf.skey --byron-genesis /home/westbam/haskell/local/byron-genesis.json --shelley-genesis /home/westbam/haskell/local/shelley-genesis.json --pool-stake $POOL_STAKE --active-stake $ACTIVE_STAKE --ledger-set prev`
+echo $BCSH | jq .
+
+EPOCH=`echo $BCSH | jq .epoch`
+echo "\`Epoch $EPOCH\` 🧙🔮:"
+
+SLOTS=`echo $BCSH | jq .epochSlots`
+IDEAL=`echo $BCSH | jq .epochSlotsIdeal`
+PERFORMANCE=`echo $BCSH | jq .maxPerformance`
+echo "\`BCSH  - $SLOTS \`🎰\`,  $PERFORMANCE% \`🍀max, \`$IDEAL\` 🧱ideal"
--- scripts/ledger-dump.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export CARDANO_NODE_SOCKET_PATH="/home/cardano-node/socket/node.socket"
-
-/usr/local/bin/cardano-cli query ledger-state --mainnet > /root/scripts/ledger-state.json
-
-exit 0
