5e962266fd14296584d03346166f2a7b64ebb590
Jason Sun <jason@sunapi386.ca>
2022-11-09 16:33:09 -0800

    Update scripts to use env HOME

--- scripts/cncli-leaderlog-current.sh
+++ scripts/cncli-leaderlog-current.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-export CARDANO_NODE_SOCKET_PATH=/home/westbam/haskell/local/db/socket
+cardano-cli query leadership-schedule \
+--mainnet \
+--genesis $CNODE_HOME/shelley-genesis.json \
+--stake-pool-id (cat $CNODE_HOME/stakepoolid.txt) \
+--vrf-signing-key-file $CNODE_HOME/vrf.skey \
+--current
 
-echo "BCSH"
-SNAPSHOT=$(/home/westbam/.local/bin/cardano-cli query stake-snapshot --stake-pool-id 00beef0a9be2f6d897ed24a613cf547bb20cd282a04edfc53d477114 --mainnet)
-/home/westbam/.cargo/bin/cncli sync --host 127.0.0.1 --port 6000 --no-service
-POOL_STAKE=$(echo "$SNAPSHOT" | grep -oP '(?<=    "poolStakeSet": )\d+(?=,?)')
-ACTIVE_STAKE=$(echo "$SNAPSHOT" | grep -oP '(?<=    "activeStakeSet": )\d+(?=,?)')
-BCSH=`/home/westbam/.cargo/bin/cncli leaderlog --pool-id 00beef0a9be2f6d897ed24a613cf547bb20cd282a04edfc53d477114 --pool-vrf-skey ./bcsh.vrf.skey --byron-genesis /home/westbam/haskell/local/byron-genesis.json --shelley-genesis /home/westbam/haskell/local/shelley-genesis.json --pool-stake $POOL_STAKE --active-stake $ACTIVE_STAKE --consensus tpraos --ledger-set current`
-echo $BCSH | jq .
-
-EPOCH=`echo $BCSH | jq .epoch`
-echo "\`Epoch $EPOCH\` 🧙🔮:"
-
-SLOTS=`echo $BCSH | jq .epochSlots`
-IDEAL=`echo $BCSH | jq .epochSlotsIdeal`
-PERFORMANCE=`echo $BCSH | jq .maxPerformance`
-echo "\`BCSH  - $SLOTS \`🎰\`,  $PERFORMANCE% \`🍀max, \`$IDEAL\` 🧱ideal"
